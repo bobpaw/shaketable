7155b447d2bbbd8f449658d621d14a2c492c49b3
ShakePi pi <pi@shakepi.kealing.austinisd.org>
2018-03-22 13:59:21 +0000

    Fixed upled to work with button and interrupt

--- upled.py
+++ upled.py
@@ -2,9 +2,22 @@
 
 import RPi.GPIO as gpio
 import time
+import random
 
-gpio.setmode(gpio.BOARD)
-gpio.setup(6, gpio.IN)
-gpio.output(6,1)
-time.sleep(5)
+def killchannel(channel):
+	gpio.output(10,0)
+gpio.setmode(gpio.BCM)
+gpio.setup(10, gpio.OUT)
+gpio.setup(9, gpio.IN)
+sttime = time.time()
+gpio.output(10,1)
+gpio.add_event_detect(9, gpio.RISING, killchannel, 100)
+sum = 0
+try:
+	while (1):
+		sum = sum + random.random()
+		print(sum)
+		time.sleep(1)
+except (KeyboardInterrupt):
+	pass
 gpio.cleanup()
