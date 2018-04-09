fbaf8662da8ec29fde51b6cdc606d579ce34cca7
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-09 14:09:48 -0500

    Correctly wrote initial pin values
    Enable is supposed to be LOW
    Sleep should be HIGH
    Reset should be HIGH
    Moved function to set step size to top
    Removed XOR'd variable and changed While loop

--- motor.py
+++ motor.py
@@ -19,56 +19,59 @@ switchleft = 12
 switchright = 20
 
 alive = True
+delaytime = .05
 
+# Interrupt to stop motor
 def stopmotor (port):
 	global alive
 	alive = False
 	print("Motor stopped")
-	gpio.output(sleep, gpio.HIGH)
+	gpio.output(sleep, gpio.LOW)
+
+# Function to write to driver for different microstep precisions
+def set_step(step, ports):
+	if not isinstance(step, str) or not isinstance(step, int):
+		return False
+	if not isinstance(step, list):
+		return False
+	if step == "full" or step == 1:
+		gpio.output(ports, gpio.LOW)
+	elif step == "half" or step == 2:
+		gpio.output(ports[0], gpio.HIGH)
+		gpio.output(ports[1:], gpio.LOW)
+	elif step == "quarter" or step == 4:
+		gpio.output(ports[0], gpio.LOW)
+		gpio.output(ports[1:], gpio.HIGH)
+	elif step == "eighth" or step == 8:
+		gpio.output(ports[:1], gpio.HIGH)
+		gpio.output(ports[2], gpio.LOW)
+	elif step == "sixteenth" or step == 16:
+		gpio.output(ports[:1], gpio.LOW)
+		gpio.output(ports[2], gpio.HIGH)
+	return True
 
 # Setup Ports
 gpio.setup([direction, step, enable, sleep, ms1, ms2, ms3], gpio.OUT)
 gpio.setup([switchright, switchleft], gpio.IN)
 gpio.output(direction, gpio.HIGH)
 gpio.output(step, gpio.LOW)
-gpio.output(enable, gpio.HIGH)
+
+# Actually turn driver on
+gpio.output(enable, gpio.LOW)
+gpio.output(sleep, gpio.HIGH)
+gpio.output(reset, gpio.HIGH)
 
 # Add interrupts for both limit switches
 gpio.add_event_detect(switchright, gpio.RISING, stopmotor, 20)
 gpio.add_event_detect(switchleft, gpio.RISING, stopmotor, 20)
 
-# Function to write to driver for different microstep precisions
-def set_step(step, ports):
-	if not isinstance(step, str):
-		return False
-	if not isinstance(step, list):
-		return False
-	if step == "full":
-		gpio.output(ports, gpio.LOW)
-	elif step == "half":
-		gpio.output(ports[0], gpio.HIGH)
-		gpio.output(ports[1:], gpio.LOW)
-	elif step == "quarter":
-		gpio.output(ports[0], gpio.LOW)
-		gpio.output(ports[1:], gpio.HIGH)
-	elif step == "eighth":
-		gpio.output(ports[:1], gpio.HIGH)
-		gpio.output(ports[2], gpio.LOW)
-	elif step == "sixteenth":
-		gpio.output(ports[:1], gpio.LOW)
-		gpio.output(ports[2], gpio.HIGH)
-	return True
-
-onoff = 0
 set_step("full", [ms1,ms2,ms3])
 try:
 	while True:
-		onoff ^= 1
-		if onoff == 1:
-			gpio.output(step, gpio.HIGH)
-		else:
-			gpio.output(step,gpio.LOW)
-		time.sleep(.05)
+		gpio.output(step, gpio.HIGH)
+		time.sleep(delaytime)
+		gpio.output(step,gpio.LOW)
+		time.sleep(delaytime)
 except KeyboardInterrupt:
 	pass
 gpio.cleanup()
